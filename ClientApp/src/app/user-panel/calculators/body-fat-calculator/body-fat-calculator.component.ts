ffca6e0754048bd87f2bb0087fc296cb960cd548
kam95oleksy <kam95oleksy@gmail.com>
2019-02-05 18:22:29 +0100

    change photos and add implementation to calculators

--- ClientApp/src/app/user-panel/calculators/body-fat-calculator/body-fat-calculator.component.ts
+++ ClientApp/src/app/user-panel/calculators/body-fat-calculator/body-fat-calculator.component.ts
@@ -13,6 +13,9 @@ export class BodyFatCalculatorComponent implements OnInit {
     waist: number,
   };
 
+  result = false;
+  bodyfat: number;
+
   constructor() {
     this.model = { sex: null, weight: null, waist: null };
    }
@@ -21,7 +24,19 @@ export class BodyFatCalculatorComponent implements OnInit {
   }
 
   onSubmit() {
+    const a = 4.15 * this.model.waist;
+    const b = a / 2.54;
+    const c = 0.082 * this.model.weight * 2.2;
+    let d;
+    if (this.model.sex === 'male') {
+      d = b - c - 98.42;
+    } else {
+      d = b - c - 76.76;
+    }
+    const e = this.model.weight * 2.2;
+    this.bodyfat = Math.round(d / e * 100);
 
+    this.result = true;
   }
 
 }
